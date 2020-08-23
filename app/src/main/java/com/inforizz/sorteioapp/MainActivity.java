08a0e90d8e8ebdcbef5e3e52fca5da536b19dac9
Giovanni <brodrigo@vectoritcgroup.com>
2020-08-23 02:11:31 -0300

    ínsert app randomico

---
+++ app/src/main/java/com/inforizz/sorteioapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.inforizz.sorteioapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortedNumber(View view){
+
+        TextView texto = findViewById(R.id.resulted);
+
+        int numero = new Random().nextInt(21);
+
+        texto.setText("O número sorteado foi: " + numero);
+    }
+}
